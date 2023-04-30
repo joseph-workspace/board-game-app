2d2e327e323ea159871f6473d17756f8e9700413
Joseph Burton <joseph.burton6967@stu.mtec.edu>
2023-04-29 19:54:48 -0600

    Part 3 completed; Application Shell

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    
+  ],
+  //exports must be placed (as below) so that other modules can access the MatToolbarModule material component(s)
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
